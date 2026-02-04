6457556c032fb43be7381c0d4fbe775fe4f566c6
hmt2k6it <24000485.tri@student.iuh.edu.vn>
2026-02-04 21:05:04 +0700

    #14 - feat: create dto for authenticate method

---
+++ src/main/java/com/example/socialnetwork/module/identity/dto/request/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.example.socialnetwork.module.identity.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthenticationRequest {
+    String username;
+    String password;
+}
