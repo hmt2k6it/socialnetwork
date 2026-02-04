4d90d94f83e33bb4d8fc20e944875c95cadc3fff
hmt2k6it <24000485.tri@student.iuh.edu.vn>
2026-02-04 19:44:30 +0700

    #6 - fix: import correct lib

--- src/main/java/com/example/socialnetwork/module/identity/entity/User.java
+++ src/main/java/com/example/socialnetwork/module/identity/entity/User.java
@@ -3,7 +3,7 @@ package com.example.socialnetwork.module.identity.entity;
 import java.util.Date;
 import java.util.Set;
 
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
