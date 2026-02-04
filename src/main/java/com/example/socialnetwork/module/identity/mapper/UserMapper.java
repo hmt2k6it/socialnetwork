70e0c2beced4bdfa24127522b4c6746fc0474514
hmt2k6it <24000485.tri@student.iuh.edu.vn>
2026-02-04 11:03:30 +0700

    #6 - feat: add method for userMapper

--- src/main/java/com/example/socialnetwork/module/identity/mapper/UserMapper.java
+++ src/main/java/com/example/socialnetwork/module/identity/mapper/UserMapper.java
@@ -2,7 +2,15 @@ package com.example.socialnetwork.module.identity.mapper;
 
 import org.mapstruct.Mapper;
 
+import com.example.socialnetwork.module.identity.dto.request.UserCreationRequest;
+import com.example.socialnetwork.module.identity.dto.response.UserResponse;
+import com.example.socialnetwork.module.identity.entity.User;
+
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
+    User toUser(UserCreationRequest request);
+
+    UserResponse toUserResponse(User save);
 
 }
