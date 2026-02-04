20c9f594cdbfd004ca7cc30481e08246ef93a38f
hmt2k6it <24000485.tri@student.iuh.edu.vn>
2026-02-04 09:02:22 +0700

    #2 - chore: add role, permission entity and repository

---
+++ src/main/java/com/example/socialnetwork/module/identity/entity/Role.java
@@ -0,0 +1,28 @@
+package com.example.socialnetwork.module.identity.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Role {
+    @Id
+    String name;
+    @ManyToMany
+    Set<Permission> permissions;
+}
