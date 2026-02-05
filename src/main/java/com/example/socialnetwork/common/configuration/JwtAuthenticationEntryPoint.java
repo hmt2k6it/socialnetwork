5674093b0bed6ee2793dfa206671f855eabe25d0
hmt2k6it <24000485.tri@student.iuh.edu.vn>
2026-02-05 22:38:34 +0700

    #25 - feat(security): implement jwt filter and utility

---
+++ src/main/java/com/example/socialnetwork/common/configuration/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,37 @@
+package com.example.socialnetwork.common.configuration;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import tools.jackson.databind.ObjectMapper;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import com.example.socialnetwork.common.dto.response.ApiResponse;
+import com.example.socialnetwork.common.exception.ErrorCode;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+        ErrorCode errorCode = ErrorCode.UNAUTHENTICATED;
+
+        response.setStatus(errorCode.getStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
+        response.flushBuffer();
+    }
+}
