e6d1d82f14b5c0b593916705caa1c3e87f186301
hmt2k6it <24000485.tri@student.iuh.edu.vn>
2026-02-06 19:46:16 +0700

    #29 - feat(service): implement logout and refresh token logic

--- src/main/java/com/example/socialnetwork/module/identity/dto/request/RefreshTokenRequest.java
+++ src/main/java/com/example/socialnetwork/module/identity/dto/request/RefreshTokenRequest.java
@@ -12,7 +12,6 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class AuthenticationRequest {
-    String username;
-    String password;
+public class RefreshTokenRequest {
+    String refreshToken;
 }
