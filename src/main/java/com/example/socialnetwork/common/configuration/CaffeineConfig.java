a1864e660ae8989ecfec1f2b4e82eb05192c5626
hmt2k6it <24000485.tri@student.iuh.edu.vn>
2026-02-07 15:37:41 +0700

    #31 - feat(infra): configure caffeine cache, security and error codes for password reset

--- src/main/java/com/example/socialnetwork/common/configuration/CaffeineConfig.java
+++ src/main/java/com/example/socialnetwork/common/configuration/CaffeineConfig.java
@@ -8,21 +8,35 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
+import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 
 @Configuration
 @EnableCaching
 public class CaffeineConfig {
     @Value("${jwt.access-token.expiration-time}")
-    private int expireAfterWrite;
+    private int accessTokenExpirationTime;
+
+    @Value("${otp.expiration-time}")
+    private int otpExpirationTime;
 
     @Bean
-    public CacheManager cacheManager() {
+    @Primary
+    CacheManager cacheManager() {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager("invalidated_tokens");
         cacheManager.setCaffeine(Caffeine.newBuilder()
-                .expireAfterWrite(expireAfterWrite, TimeUnit.SECONDS)
+                .expireAfterWrite(accessTokenExpirationTime, TimeUnit.SECONDS)
                 .maximumSize(10000));
         return cacheManager;
+    }
+
+    @Bean
+    Cache<String, String> otpCache() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(otpExpirationTime, TimeUnit.SECONDS)
+                .maximumSize(1000)
+                .build();
     }
 }
