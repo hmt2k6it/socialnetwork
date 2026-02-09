3f06c3b30141a29107d63c20729b7f33121d77f5
hmt2k6it <24000485.tri@student.iuh.edu.vn>
2026-02-09 23:01:39 +0700

    #37 - feat(service): implement Role service with RBAC and transaction management

---
+++ src/main/java/com/example/socialnetwork/module/identity/service/RoleService.java
@@ -0,0 +1,27 @@
+package com.example.socialnetwork.module.identity.service;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import com.example.socialnetwork.module.identity.dto.request.RoleCreationRequest;
+import com.example.socialnetwork.module.identity.dto.response.RoleResponse;
+import com.example.socialnetwork.module.identity.entity.Permission;
+
+public interface RoleService {
+    @PreAuthorize("hasRole('ADMIN')")
+    List<RoleResponse> getAllRoles();
+
+    @PreAuthorize("hasRole('ADMIN')")
+    RoleResponse getRoleById(String id);
+
+    @PreAuthorize("hasRole('ADMIN')")
+    RoleResponse createRole(RoleCreationRequest role);
+
+    @PreAuthorize("hasRole('ADMIN')")
+    String deleteRole(String id);
+
+    @PreAuthorize("hasRole('ADMIN')")
+    RoleResponse assignPermissionToRole(String roleId, Set<Permission> permissions);
+}
---
+++ src/main/java/com/example/socialnetwork/module/identity/service/impl/RoleServiceImpl.java
@@ -0,0 +1,64 @@
+package com.example.socialnetwork.module.identity.service.impl;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.socialnetwork.common.exception.AppException;
+import com.example.socialnetwork.common.exception.ErrorCode;
+import com.example.socialnetwork.module.identity.dto.request.RoleCreationRequest;
+import com.example.socialnetwork.module.identity.dto.response.RoleResponse;
+import com.example.socialnetwork.module.identity.entity.Permission;
+import com.example.socialnetwork.module.identity.entity.Role;
+import com.example.socialnetwork.module.identity.mapper.RoleMapper;
+import com.example.socialnetwork.module.identity.repository.RoleRepository;
+import com.example.socialnetwork.module.identity.service.RoleService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class RoleServiceImpl implements RoleService {
+    RoleRepository roleRepository;
+    RoleMapper roleMapper;
+
+    @Override
+    public List<RoleResponse> getAllRoles() {
+        return roleRepository.findAll().stream().map(arg0 -> roleMapper.toRoleResponse(arg0)).toList();
+    }
+
+    @Override
+    public RoleResponse getRoleById(String id) {
+        Role role = roleRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorCode.ROLE_NOT_FOUND));
+        return roleMapper.toRoleResponse(role);
+    }
+
+    @Override
+    @Transactional
+    public RoleResponse createRole(RoleCreationRequest request) {
+        Role role = roleMapper.toRole(request);
+        return roleMapper.toRoleResponse(roleRepository.save(role));
+    }
+
+    @Override
+    @Transactional
+    public String deleteRole(String id) {
+        roleRepository.deleteById(id);
+        return "Role deleted successfully";
+    }
+
+    @Override
+    @Transactional
+    public RoleResponse assignPermissionToRole(String id, Set<Permission> permissions) {
+        Role role = roleRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorCode.ROLE_NOT_FOUND));
+        role.setPermissions(permissions);
+        return roleMapper.toRoleResponse(roleRepository.save(role));
+    }
+
+}
