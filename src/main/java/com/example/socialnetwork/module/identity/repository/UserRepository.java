ae03442ce2bbfeca7198350efec42c2aff2f9667
hmt2k6it <24000485.tri@student.iuh.edu.vn>
2026-02-11 22:28:31 +0700

    #37 - feat(identity): update repositories and optimize user query

--- src/main/java/com/example/socialnetwork/module/identity/repository/UserRepository.java
+++ src/main/java/com/example/socialnetwork/module/identity/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.socialnetwork.module.identity.repository;
 
 import java.util.Optional;
 
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,9 +11,13 @@ import com.example.socialnetwork.module.identity.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+    @Override
+    @EntityGraph(attributePaths = {"roles", "roles.permissions"})
+    @NonNull
+    Optional<User> findById(String id);
 
     boolean existsByUsername(String username);
-
+    @EntityGraph(attributePaths = {"roles", "roles.permissions"})
     Optional<User> findByUsername(String username);
 
     boolean existsByEmail(String email);
